7bcbb69d08ddc85768634bb0cfcbaa53b996bedc
Frecy16 <1376210796@qq.com>
2021-11-16 21:55:01 +0800

    修改test文件

--- test.py
+++ test.py
@@ -28,9 +28,9 @@ class TestUserModel():
         assert u.check_password('dog') == False
         assert u.check_password('cat') == True
 
-    # def test_ava(self):
-    #     u = User(username='john', email='john@example.com')
-    #     assert u.avatar(128) == "'https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6?d=identicon&s=128'"
+    def test_ava(self):
+        u = User(username='john', email='john@example.com')
+        assert u.avatar(128) == "'https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6?d=identicon&s=128'"
 
     def test_follow(self):
         u1 = User(username='joson', email='joson@example.com')
